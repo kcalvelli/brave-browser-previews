64e082d28104dc6bf8ff44553739a1b692cd11af
kcalvelli <1329446+kcalvelli@users.noreply.github.com>
2026-01-01 04:20:18 +0000

    chore: update brave versions

--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -1,7 +1,7 @@
 { callPackage, fetchurl }:
 let
-  version = "1.87.132";
-  hash = "16m89yv7q9xxy0006k12qjg6wwbplndrgmxc946gpmgnfx4lmgg0";
+  version = "1.87.134";
+  hash = "0j4yz0fixpw2fyfk64msw5wac63mdb14530c82jq0kcv6sm213q7";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-nightly";
