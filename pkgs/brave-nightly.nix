009749a951884ecd3d3314241ae4fa564ce32b5c
kcalvelli <1329446+kcalvelli@users.noreply.github.com>
2026-05-23 05:12:00 +0000

    chore: update brave versions

--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -1,11 +1,11 @@
 { callPackage, fetchurl }:
 let
-  version = "1.92.88";
-  hash = "0b6rwsnk5m5vsv88g7y0p33c4r69d6n9sd59x68230l1qi9n3aq0";
+  version = "1.92.91";
+  hash = "0ywhyjq45fpydhl7xxalqlfr2cpw7wqfalajkql40pr4jsaihy5a";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-nightly";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.92.88/brave-browser-nightly_1.92.88_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.92.91/brave-browser-nightly_1.92.91_amd64.deb";
   commandLineArgs = "--enable-features=BraveAIChatAgentProfile";
 }
--- pkgs/brave-origin-nightly.nix
+++ pkgs/brave-origin-nightly.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl }:
 let
-  version = "1.92.85";
-  hash = "1s9bsqnxzq828mb8j618b4457yx4845gji716wym55bwd91vn4wg";
+  version = "1.92.89";
+  hash = "0xhf4f559z7vh440yb0bwa2kiginzvi4wj6qy5645k28wg4213df";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-origin-nightly";
   inherit version hash;
-  url = "https://brave-browser-apt-nightly.s3.brave.com/pool/main/b/brave-origin-nightly/brave-origin-nightly_1.92.85_amd64.deb";
+  url = "https://brave-browser-apt-nightly.s3.brave.com/pool/main/b/brave-origin-nightly/brave-origin-nightly_1.92.89_amd64.deb";
 }
