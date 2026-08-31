0f62d4937ff050a12eac0cfcaac83f5e30356f58
kcalvelli <1329446+kcalvelli@users.noreply.github.com>
2026-08-31 10:11:38 +0000

    chore: update brave versions

--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -1,11 +1,11 @@
 { callPackage, fetchurl }:
 let
-  version = "1.96.29";
-  hash = "1imzix3kkh9g87682y1irm2w5sdms44ssw6wia7m4x0rpla3z71k";
+  version = "1.96.30";
+  hash = "0ja65lv1iq7cb22sc9rjzrnga2h248b5m4gyxpmfl3si2imfp59b";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-nightly";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.96.29/brave-browser-nightly_1.96.29_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.96.30/brave-browser-nightly_1.96.30_amd64.deb";
   commandLineArgs = "--enable-features=BraveAIChatAgentProfile";
 }
