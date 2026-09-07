0ee83f367afa301ccbfd4c7bfb920feb77af1e36
kcalvelli <1329446+kcalvelli@users.noreply.github.com>
2026-09-07 08:42:13 +0000

    chore: update brave versions

--- pkgs/brave-beta.nix
+++ pkgs/brave-beta.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl }:
 let
-  version = "1.96.46";
-  hash = "1amkkqy2fkc8lawighb5sxryypyzjl69hqpqrjls2ila2i60kg9i";
+  version = "1.96.47";
+  hash = "1k5k6y7d8ddnhvl9cjgzarpnbjl2ni1600rv8lwagj5w68d7pig1";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-beta";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.96.46/brave-browser-beta_1.96.46_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.96.47/brave-browser-beta_1.96.47_amd64.deb";
 }
--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -1,11 +1,11 @@
 { callPackage, fetchurl }:
 let
-  version = "1.97.8";
-  hash = "0bjgzmrbxyg2yp012gi0v09ic65ji0brw0ggqc7gdafyv1wx6a14";
+  version = "1.97.9";
+  hash = "0jkznlf1v25lyqh56nbyvq2mdg1ymjl18rn6m3aj9yarnzh5f67a";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-nightly";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.97.8/brave-browser-nightly_1.97.8_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.97.9/brave-browser-nightly_1.97.9_amd64.deb";
   commandLineArgs = "--enable-features=BraveAIChatAgentProfile";
 }
